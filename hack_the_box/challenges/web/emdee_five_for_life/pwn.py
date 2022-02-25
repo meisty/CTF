d4968ae7cfc128bc55d57155f0636a48e8dd4f50
meisty <shaundixon666@gmail.com>
2022-02-25 09:32:56 +0000

    Added comments to script to explain usage.

--- hack_the_box/challenges/web/emdee_five_for_life/pwn.py
+++ hack_the_box/challenges/web/emdee_five_for_life/pwn.py
@@ -3,6 +3,11 @@ import sys
 from bs4 import BeautifulSoup
 import hashlib
 
+# Script to grab html from website, extract string and then MD5hash
+# Then post back to website to retrieve flag in response text
+# Script usage: python3 pwn.py <url_of_challenge_site>
+
+# Create a session to use with requests
 req = requests.session()
 url = sys.argv[1]
 r = req.get(url)
